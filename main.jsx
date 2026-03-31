7bd4390b4e5a2f319025cc318d6482c0151ca472
poole00000 <poole00000@gmail.com>
2026-03-31 15:25:09 -0400

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import OpspecsPortal from "./App";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <OpspecsPortal />
-  </React.StrictMode>
-);
